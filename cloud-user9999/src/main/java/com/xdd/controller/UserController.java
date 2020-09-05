f4bbf8dffbdc0f3b5f99d5003d4868935537db5c
felixalle <davic2007@qq.com>
2020-09-05 16:27:53 +0800

    赵小涵修改 version 2.0 新增修改功能

--- cloud-user9999/src/main/java/com/xdd/controller/UserController.java
+++ cloud-user9999/src/main/java/com/xdd/controller/UserController.java
@@ -3,9 +3,9 @@ package com.xdd.controller;
 import com.xdd.service.UserService;
 import com.xld.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("user")
@@ -14,14 +14,53 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("myInfo/{id}")//通过 id 查数据库，显示该用户所有信息
+    //restful风格的地址
+    @GetMapping("myInfo/{id}")//通过 id 查数据库，显示该用户所有信息
     public User myInfo(@PathVariable("id") Integer id) {
         return userService.getMyInfo(id);
     }
 
-    @RequestMapping("test")
-    public String test() {
-        return "This is a test";
+
+
+
+    //测试!!!
+    @PostMapping("test1")
+    public void test1(@RequestBody User user) {
+        System.out.println(user.getTel());
     }
+    @PostMapping("test2")
+    public void test2(String str) {
+        System.out.println(str);
+    }
+
+
+
+    //非restful风格的地址
+    @PostMapping("updateAll")//修改全部信息
+    public void updateAll(@RequestBody User user) {
+        userService.updateAll(user);
+    }
+
+    @PostMapping("updateAvatar")//修改头像
+    public void updateAvatar(@RequestParam("avatar") String avatar, @RequestParam("id") Integer id) {
+        userService.updateAvatar(avatar, id);
+    }
+
+    @PostMapping("updateNickname")//修改昵称
+    public void updateNickname(@RequestParam("nickname") String nickname, @RequestParam("id") Integer id) {
+        userService.updateNickname(nickname, id);
+    }
+
+    @PostMapping("updateSex")//修改性别
+    public void updateAvatar(@RequestParam("sex") Integer sex, @RequestParam("id") Integer id) {
+        userService.updateSex(sex, id);
+    }
+
+    @PostMapping("updateBirthday")//修改生日
+    public void updateBirthday(@RequestParam("birthday") Date birthday, @RequestParam("id") Integer id) {
+        userService.updateBirthday(birthday, id);
+    }
+
+
 
 }
--- cloud-user9999/src/main/java/com/xdd/service/impl/UserServiceImpl.java
+++ cloud-user9999/src/main/java/com/xdd/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.xld.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -15,5 +17,30 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getMyInfo(Integer id) {
         return userMapper.getMyInfo(id);
+    }
+
+    @Override
+    public void updateAll(User user) {
+        userMapper.updateAll(user);
+    }
+
+    @Override
+    public void updateAvatar(String avatar, Integer id) {
+        userMapper.updateAvatar(avatar, id);
+    }
+
+    @Override
+    public void updateNickname(String nickname, Integer id) {
+        userMapper.updateNickname(nickname, id);
+    }
+
+    @Override
+    public void updateSex(Integer sex, Integer id) {
+        userMapper.updateSex(sex, id);
+    }
+
+    @Override
+    public void updateBirthday(Date birthday, Integer id) {
+        userMapper.updateBirthday(birthday, id);
     }
 }

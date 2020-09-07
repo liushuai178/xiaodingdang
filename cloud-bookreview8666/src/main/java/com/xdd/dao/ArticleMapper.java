919641cadf4bb59c3c0b93244538639f6e4694f9
tianwen945 <1599878361@qq.com>
2020-09-07 16:42:57 +0800

    文章功能

---
+++ cloud-bookreview8666/src/main/java/com/xdd/dao/ArticleMapper.java
@@ -0,0 +1,18 @@
+package com.xdd.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xld.pojo.Article;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author tianwen
+ * @since 2020-09-05
+ */
+@Repository
+public interface ArticleMapper extends BaseMapper<Article> {
+
+}
